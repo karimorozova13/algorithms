2748c77104a6c8659628c38b8468ba72e4e3a448
karimorozova13 <k.morozova@tiomarkets.com>
2024-03-04 14:50:18 +0200

    binary tree, avl tree

--- module7/binary_search_tree.py
+++ module7/binary_search_tree.py
@@ -3,6 +3,13 @@ class Node:
         self.left = None
         self.right =  None
         self.val = key
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\t" * level + prefix + str(self.val) + "\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
         
 def insert(root, key):
     if root is None:
@@ -20,6 +27,46 @@ def search(root, key):
         return search(root.left, key)
     else:
         return search(root.right, key)
+ 
+def min_val_node(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current   
+
+
+def delete(root, key):
+    if not root:
+        return root
+    
+    if key < root.val:
+        print('key < root.val', key, 'key - ', root.val)
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        print('key > root.val', key, 'key - ', root.val)
+        
+        root.right = delete(root.right, key)
+    else:
+        if not root.left:
+            
+            temp = root.right
+            root = None
+            print('not root.left', temp, 'temp - ')
+            
+            return temp
+        elif not root.right:
+            temp = root.left
+            root = None
+            print('not root.right', temp, 'temp - ')
+            
+            return temp
+        root.val = min_val_node(root.right).val
+        print('root.val = min_val_node(root.right).val', root.val)
+        
+        root.right = delete(root.right, root.val)
+        print(root)
+        
+    return root
     
 root = Node(5)
 root = insert(root, 3)
@@ -30,3 +77,5 @@ root = insert(root, 6)
 root = insert(root, 8)
 
 print(search(root, 7).val)
+root = delete(root, 7)
+print(root)
